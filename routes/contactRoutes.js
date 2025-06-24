cdff7c47e369513f6608a7b2713fe7080c195100
masxkip <jesufemiketiku@gmail.com>
2025-06-24 04:01:47 -0700

    update back

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { sendContactForm } = require('../controllers/contactController');
 
-
+// POST /api/contact
 router.post('/contact', sendContactForm);
 
 module.exports = router;
